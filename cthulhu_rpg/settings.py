abeeb182f1d0ccf92594978215b869dd76bcf6a1
ypurek <oleksii.ostapov@gmail.com>
2026-05-04 20:25:21 +0200

    updated the security settings

--- cthulhu_rpg/settings.py
+++ cthulhu_rpg/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s7+ef!!ij@s2men*&l1&8yfq&%fp**r!ajofk0ijkn))8xpiq@'
+SECRET_KEY = os.getenv('DJANGO_SECRET', default='debug secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "True") in ('True', 'true', '1')
 
 ALLOWED_HOSTS = []
 
